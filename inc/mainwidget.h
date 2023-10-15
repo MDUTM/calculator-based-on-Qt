0a9d5ce7ac43ad83e28b5af4f14458aef40d6b52
MDUTM <MDUTM@outlook.com>
2023-10-15 17:02:51 +0800

    修复界面存在的bug

--- inc/mainwidget.h
+++ inc/mainwidget.h
@@ -15,6 +15,47 @@ public:
     MainWidget(QWidget *parent = nullptr);
     ~MainWidget();
 
+private slots:
+    void on_btn_1_clicked();
+
+    void on_num_0_clicked();
+
+    void on_btn_point_clicked();
+
+    void on_btn_ret_clicked();
+
+    void on_num_1_clicked();
+
+    void on_num_2_clicked();
+
+    void on_num_3_clicked();
+
+    void on_btn_add_clicked();
+
+    void on_num_4_clicked();
+
+    void on_num_5_clicked();
+
+    void on_num_6_clicked();
+
+    void on_btn_sub_clicked();
+
+    void on_num_7_clicked();
+
+    void on_num_8_clicked();
+
+    void on_num_9_clicked();
+
+    void on_btn_pow_clicked();
+
+    void on_btn_mod_clicked();
+
+    void on_btn_mul_clicked();
+
+    void on_btn_clear_clicked();
+
+    void on_btn_del_clicked();
+
 private:
     Ui::MainWidget *ui;
 };
--- src/mainwidget.cpp
+++ src/mainwidget.cpp
@@ -13,3 +13,122 @@ MainWidget::~MainWidget()
     delete ui;
 }
 
+void MainWidget::on_btn_1_clicked()
+{
+
+}
+
+
+void MainWidget::on_num_0_clicked()
+{
+
+}
+
+
+void MainWidget::on_btn_point_clicked()
+{
+
+}
+
+
+void MainWidget::on_btn_ret_clicked()
+{
+
+}
+
+
+void MainWidget::on_num_1_clicked()
+{
+
+}
+
+
+void MainWidget::on_num_2_clicked()
+{
+
+}
+
+
+void MainWidget::on_num_3_clicked()
+{
+
+}
+
+
+void MainWidget::on_btn_add_clicked()
+{
+
+}
+
+
+void MainWidget::on_num_4_clicked()
+{
+
+}
+
+
+void MainWidget::on_num_5_clicked()
+{
+
+}
+
+
+void MainWidget::on_num_6_clicked()
+{
+
+}
+
+
+void MainWidget::on_btn_sub_clicked()
+{
+
+}
+
+
+void MainWidget::on_num_7_clicked()
+{
+
+}
+
+
+void MainWidget::on_num_8_clicked()
+{
+
+}
+
+
+void MainWidget::on_num_9_clicked()
+{
+
+}
+
+
+void MainWidget::on_btn_pow_clicked()
+{
+
+}
+
+
+void MainWidget::on_btn_mod_clicked()
+{
+
+}
+
+
+void MainWidget::on_btn_mul_clicked()
+{
+
+}
+
+
+void MainWidget::on_btn_clear_clicked()
+{
+
+}
+
+
+void MainWidget::on_btn_del_clicked()
+{
+
+}
+
