6847a9365ff29ccfec342df1140c12dd11fa2446
MDUTM <MDUTM@outlook.com>
2023-10-16 20:20:50 +0800

    优化算法

--- src/mainwidget.cpp
+++ src/mainwidget.cpp
@@ -55,70 +55,72 @@ void MainWidget::on_btn_ret_clicked()
     qsizetype len = val.length();
     char *buf = new char[len]();
     strcpy(buf, val.toStdString().data());
-    char *opera = nullptr;
-    double retVal;
 
-    // 识别操作
-    if (opera = strstr(buf, "+"))
+    char opera = 0;
+    double val1 = 0;
+    double val2 = 0;
+    int flags = 1;
+
+    for (int i = 0; i < strlen(buf); i++)
+        if (!isdigit(buf[i]))
+        {
+            opera = buf[i];
+            flags += i;
+            break;
+        }
+    double retVal;
+    val1 = atof(buf);
+    val2 = atof(buf + flags);
+
+    if (val2 == 0)
     {
-        double valA = atof(buf);
-        double valB = atof(opera + 1);
-        retVal = valA + valB;
-    }
-    else if (opera = strstr(buf, "-"))
-    {
-        double valA = atof(buf);
-        double valB = atof(opera + 1);
-        retVal = valA - valB;
-    }
-    else if (opera = strstr(buf, "*"))
-    {
-        double valA = atof(buf);
-        double valB = atof(opera + 1);
-        retVal = valA * valB;
-    }
-    else if (opera = strstr(buf, "/"))
-    {
-        double valA = atof(buf);
-        double valB = atof(opera + 1);
-        retVal = valA / valB;
-    }
-    else if (opera = strstr(buf, "%"))
-    {
-        int valA = atoi(buf);
-        int valB = atoi(opera + 1);
-        retVal = (valA % valB);
-    }
-    else if (opera = strstr(buf, "^"))
-    {
-        double valA = atof(buf);
-        double valB = atof(opera + 1);
-        retVal = pow(valA, valB);
-    }
-    else
-    {
-        delete[] buf;
+        ui->label->setText(buf);
+        val.clear();
+        memset(buf + flags, 0, strlen(buf) - flags);
+        sprintf(buf, "%lf", retVal);
+        removeZero(buf);
+        val = buf;
+
+        ui->lineEdit->setText(val);
         return;
     }
 
-    // 只做两个数的运算
+    if (opera == '+')
     {
-        int count = 0;
-        for (int i = 0; i < strlen(buf); i++)
-        {
-            if (buf[i] == '+' || buf[i] == '-'
-                || buf[i] == '*' || buf[i] == '/'
-                || buf[i] == '%' || buf[i] == '^')
-                count++;
-            if (count == 2)
-            {
-                memset(buf + i, 0, len - i);
-                break;
-            }
-        }
+        retVal = val1 + val2;
+        memset(buf, 0, len);
+        sprintf(buf, "%lf+%lf", val1, val2);
     }
-
-    //
+    else if (opera == '-')
+    {
+        retVal = val1 - val2;
+        memset(buf, 0, len);
+        sprintf(buf, "%lf-%lf", val1, val2);
+    }
+    else if (opera == '*')
+    {
+        retVal = val1 * val2;
+        memset(buf, 0, len);
+        sprintf(buf, "%lf*%lf", val1, val2);
+    }
+    else if (opera == '/')
+    {
+        retVal = val1 / val2;
+        memset(buf, 0, len);
+        sprintf(buf, "%lf/%lf", val1, val2);
+    }
+    else if (opera == '^')
+    {
+        retVal = pow(val1, val2);
+        memset(buf, 0, len);
+        sprintf(buf, "%lf^%lf", val1, val2);
+    }
+    else if (opera == '%')
+    {
+        retVal = (int)val1 % (int)val2;
+        memset(buf, 0, len);
+        sprintf(buf, "%lf%%%lf", val1, val2);
+    }
 
     ui->label->setText(buf);
     memset(buf, 0, len);
@@ -150,8 +152,20 @@ void MainWidget::on_num_3_clicked()
 
 void MainWidget::on_btn_add_clicked()
 {
-    val += "+";
+    // qsizetype len = val.length();
+    // char *buf = new char[len]();
+    // strcpy(buf, val.toStdString().data());
+    // if (isdigit(buf[len]))
+        val += "+";
+    // else
+    // {
+    //     val = val.remove(val.length() - 1, 1);
+    //     val += "+";
+    // }
+
     ui->lineEdit->setText(val);
+
+    // delete[] buf;
 }
 
 void MainWidget::on_num_4_clicked()
@@ -174,8 +188,15 @@ void MainWidget::on_num_6_clicked()
 
 void MainWidget::on_btn_sub_clicked()
 {
+    // qsizetype len = val.length();
+    // char *buf = new char[len]();
+    // strcpy(buf, val.toStdString().data());
+    // if (!isdigit(buf[len]))
+    //     val = val.remove(val.length() - 1, 1);
+
     val += "-";
     ui->lineEdit->setText(val);
+    // delete[] buf;
 }
 
 void MainWidget::on_num_7_clicked()
@@ -198,20 +219,41 @@ void MainWidget::on_num_9_clicked()
 
 void MainWidget::on_btn_pow_clicked()
 {
+    // qsizetype len = val.length();
+    // char *buf = new char[len]();
+    // strcpy(buf, val.toStdString().data());
+    // if (!isdigit(buf[len]))
+    //     val = val.remove(val.length() - 1, 1);
+
     val += "^";
     ui->lineEdit->setText(val);
+    // delete[] buf;
 }
 
 void MainWidget::on_btn_mod_clicked()
 {
+    // qsizetype len = val.length();
+    // char *buf = new char[len]();
+    // strcpy(buf, val.toStdString().data());
+    // if (!isdigit(buf[len]))
+    //     val = val.remove(val.length() - 1, 1);
+
     val += "%";
     ui->lineEdit->setText(val);
+    // delete[] buf;
 }
 
 void MainWidget::on_btn_mul_clicked()
 {
+    // qsizetype len = val.length();
+    // char *buf = new char[len]();
+    // strcpy(buf, val.toStdString().data());
+    // if (!isdigit(buf[len]))
+    //     val = val.remove(val.length() - 1, 1);
+
     val += "*";
     ui->lineEdit->setText(val);
+    // delete[] buf;
 }
 
 void MainWidget::on_btn_del_clicked()
@@ -222,8 +264,15 @@ void MainWidget::on_btn_del_clicked()
 
 void MainWidget::on_btn_div_clicked()
 {
+    // qsizetype len = val.length();
+    // char *buf = new char[len]();
+    // strcpy(buf, val.toStdString().data());
+    // if (!isdigit(buf[len]))
+    //     val = val.remove(val.length() - 1, 1);
+
     val += "/";
     ui->lineEdit->setText(val);
+    // delete[] buf;
 }
 
 void MainWidget::on_btn_clear_clicked()
